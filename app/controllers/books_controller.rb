0b3ebe492f5723a5c7bfb830e02d99a5f0e6ec88
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-05 09:24:00 +0000

    [add]レイアウト作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,33 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path(@book.id)
   end
 
   def show
+  end
+
+  def edit
+    
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.params(book_params)
+    redirect_to users_path
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
+  get "homes/about" => "homes#about",as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:new, :index, :show]
-  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit]
+  resources :users, only: [:new, :index, :show, :edit, :update]
 end
