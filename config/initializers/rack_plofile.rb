a5c95773cd102e63696af9cf91dae33d46745467
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-10 02:49:40 +0000

    [finish]Bookers2

--- config/initializers/rack_plofile.rb
+++ config/initializers/rack_plofile.rb
@@ -1,1 +1,7 @@
-Rack::MiniProfiler.config.position = 'bottom-right'
+
+if Rails.env.development?
+  require 'rack-mini-profiler'
+  Rack::MiniProfiler.config.position = 'bottom-right'
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+end
